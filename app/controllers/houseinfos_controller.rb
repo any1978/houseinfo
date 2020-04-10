6040f191620b089a362fc647b7101fb93d27d1c0
katsuhiro.tsukinoki@gmail.com <slowandlow3104>
2020-04-10 16:50:08 +0900

    レイアウト調整

--- app/controllers/houseinfos_controller.rb
+++ app/controllers/houseinfos_controller.rb
@@ -1,14 +1,14 @@
 class HouseinfosController < ApplicationController
-    before_action only: [:show, :edit, :new, :update, :destroy]
+    # before_action :set_houseinfo,only: [:show, :edit, :new, :update, :destroy]
   
     def index
       @houseinfos = Houseinfo.all
     end
   
     def new
-        @houseinfo = Houseinfo.new
-        2.times { @houseinfo.nearstations.build }
-        # @houseinfo.nearstation.build
+      @houseinfo = Houseinfo.new
+      2.times { @houseinfo.nearstations.build }
+      # @houseinfo.nearstation.build
     end
   
     def create
@@ -23,39 +23,46 @@ class HouseinfosController < ApplicationController
     
     
     def show
-      @nearstations = @houseinfo.nearstations
+      @houseinfo = Houseinfo.find(params[:id])
+      # @houseinfo.nearstations.build
+      # 2.times { @houseinfo.nearstations.build }
+      # @nearstations = @houseinfo.nearstations
     end
   
     def edit
+      @houseinfo = Houseinfo.find(params[:id])
       @houseinfo.nearstations.build
+      # 2.times { @houseinfo.nearstations.build }
+      # redirect_to houseinfos_path, notice: "物件情報を編集しました！"
     end
 
     def update
-      # @blog = Blog.find(params[:id])
+      @houseinfo = Houseinfo.find(params[:id])
       if @houseinfo.update(houseinfo_params)
-        redirect_to houseinfo_path, notice: "物件情報を編集しました！"
+        redirect_to houseinfos_path, notice: "物件情報を編集しました！"
       else
         render :edit
       end
     end
     
     def destroy
+      @houseinfo = Houseinfo.find(params[:id])
       @houseinfo.destroy
-      redirect_to houseinfo_path, notice:"物件情報を削除しました！"
+      redirect_to houseinfos_path, notice:"物件情報を削除しました！"
     end
     
-    def confirm
-      @houseinfo = Houseinfo.new(houseinfo_params)
-      render :new if @houseinfo.invalid?
-      # redirect_to user_path(current_user.id), notice: "ブログを編集しました！" if @blog.invalid?   
-    end 
+    # def confirm
+    #   @houseinfo = Houseinfo.new(houseinfo_params)
+    #   render :new if @houseinfo.invalid?
+    #   # redirect_to user_path(current_user.id), notice: "ブログを編集しました！" if @blog.invalid?   
+    # end 
 
   
     private
   
     def houseinfo_params
       params.require(:houseinfo).permit(:name, :rent, :address, :age, :note, 
-                nearstation_attributes: [:routename, :station, :minutes, :id, :destroy])
+                nearstations_attributes: [:routename, :station, :minutes, :id, :destroy])
     end
   
     # idをキーとして値を取得するメソッドを追加
--- app/helpers/houseinfos_helper.rb
+++ app/helpers/houseinfos_helper.rb
@@ -1,10 +1,17 @@
 module HouseinfosHelper
     def choose_new_or_edit
         if action_name == 'new'
-          confirm_houseinfos_path
+          houseinfos_path
         elsif action_name == 'edit'
           houseinfo_path
         end
-      end
+    end
 end
 
+# def choose_new_or_edit
+#   if action_name == 'new'
+#     houseinfos_path
+#   elsif action_name == 'edit'
+#     houseinfos_path
+#   end
+# end
